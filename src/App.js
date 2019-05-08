547dfa7c470cb82917c525ae2d8de10ee1bf5ce6
Elizabeth Asbell <e.asbell8@gmail.com>
2019-05-08 16:42:04 -0600

    Add development environment variable

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      projects: [],
+      palettes: []
+    }
+  }
+
+  componentDidMount = async () => {
+    const url = process.env.REACT_APP_BACKEND_URL + '/api/v1/projects'
+    const response = await fetch(url)
+    const data = response.json()
+    console.log(data)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>Palette Picker</h1>
+          {/* generate button */}
+          {/* projects button */}
+        </header>
+        {/* random palette */}
+        {/* save palette */}
+      </div>
+    )
+  }
 }
 
 export default App;
