6fbe2d311197e3660471cefcd772174b9baa2fd8
Taylor Sperry <sperry.taylor@gmail.com>
2019-05-12 09:11:18 -0600

    Style project display page

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import './Project.css'
+import PaletteDisplay from '../PaletteDisplay/PaletteDisplay'
 
 export const Project = (props) => {
     const { name, id } = props.project
     const palettes = props.palettes.filter(palette => {
       return palette.project_id === id
     }).map(palette => {
-      return <div key={palette.id} className='palette-display'>         {palette.palette_name}<div style={{backgroundColor: palette.color_1}}>{palette.color_1}</div><div style={{backgroundColor: palette.color_2}}>{palette.color_2}</div><div style={{backgroundColor: palette.color_3}}>{palette.color_3}</div><div style={{backgroundColor: palette.color_4}}>{palette.color_4}</div><div style={{backgroundColor: palette.color_5}}>{palette.color_5}</div>
-      </div>
+      return <PaletteDisplay key={palette.id} palette={palette}/>
     })
 
     return(
