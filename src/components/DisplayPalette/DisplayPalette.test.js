64b96d98b9204a57896273055da14972bbe5f6ff
Taylor Sperry <sperry.taylor@gmail.com>
2019-05-13 14:16:52 -0600

    add DisplayPalette test

--- src/components/DisplayPalette/DisplayPalette.test.js
+++ src/components/DisplayPalette/DisplayPalette.test.js
@@ -1,23 +1,23 @@
 import React from 'react'
 import { DisplayPalette } from './DisplayPalette'
-import { shallow } from 'enzyme'
-// Enzyme.configure({ adapter: new Adapter() })
+import { shallow, configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
 
 describe('DisplayPalette', () => {
   let wrapper
   let mockPalette = {
-    id: 4,
     palette_name: 'my palette',
     color_1: 'blue',
     color_2: 'pink',
     color_3: 'yellow',
     color_4: 'green',
-    color_5: 'red',
-    project_id: 7
+    color_5: 'red'
   }
 
   it('should match the snapshot', () => {
-    wrapper = shallow(<DisplayPalette {...mockPalette} />)
+    wrapper = shallow(<DisplayPalette palette={mockPalette} />)
     expect(wrapper).toMatchSnapshot()
   })
 })
